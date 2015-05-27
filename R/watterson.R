0ae1a2fb0f795eff70027f515147e88e21dbf846
Per Unneberg <per.unneberg@scilifelab.se>
2015-05-27 21:45:52 +0200

    Release 0.2.0
    * DESCRIPTION: change package name
    * NAMESPACE: load dynamic library
    * R/coalesce.R: add L parameter
    * src/tree.c: fix delete_node
    * src/tree.h: add state

---
+++ R/watterson.R
@@ -0,0 +1,33 @@
+#' watterson estimator
+#'
+#' @param int Sn number of segregating sites
+#' @param int n number of samples
+#'
+#' @return theta_w
+#'
+watterson <- function(Sn, n) {
+    Sn / sum(1/seq(1, n-1))
+}
+#' watterson estimator for several fragments
+#'
+#' @param int x number of fragments
+#' @param int n sample size
+#' @param int L fragment size
+#' @param double theta per site mutation rate
+#'
+#' @return theta_w for each fragment
+#'
+estimate_watterson <- function(x=10, n=100, L=200, theta=0.01) {
+    # NB: fragment size is not used in coalescent function as we're
+    # not generating the character states
+    d <- as.data.frame(
+        do.call("rbind",
+                lapply(seq(1, x),
+                       function(y) {
+                           c(fragment=y, coalescent(n=n, theta = theta*L))
+                       }
+                       ))
+    )
+    theta_w = tapply(d$nmut, d$fragment, watterson, n)
+    cbind(d, theta=theta_w)
+}
